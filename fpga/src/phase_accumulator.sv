a4b5a30a5014a710f429401b0c31022c85948d60
Lukas <luka.varhol@gmail.com>
2026-02-01 15:50:06 +0100

    [refactor]: created vivado porject

---
+++ fpga/src/phase_accumulator.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.02.2026 15:26:44
+// Design Name: 
+// Module Name: phase_accumulator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module phase_accumulator(
+
+    );
+endmodule
